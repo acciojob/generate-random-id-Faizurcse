ef28f90f2c6e4e2f18bba96a42882c83255507a3
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-12 02:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function makeid(l) {
 	 let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < l; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
